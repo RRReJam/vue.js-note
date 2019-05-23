dcf08c6354b1f901dae27db203f71b4d6a5c4022
RRReJam <297884048@qq.com>
2019-05-23 22:15:20 +0800

    1~

---
+++ 1/js/myjs 03.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        url:'www.baidu.com',
+        src:"https://yuba.douyu.com/api/dy/anchor/anchorTopic?room_id=532152",
+        klass:"btn",
+        isActive:true,
+    }
+})
